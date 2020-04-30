e1387b7bf3ff177f212c7de875e05bce5d2e94a2
Igor Selin <lifesellin@gmail.com>
2020-04-30 23:14:10 +0300

    New desing

--- src/contonents/Havbar/Nav.jsx
+++ src/contonents/Havbar/Nav.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import s from './Nav.module.css'
 import {NavLink} from "react-router-dom";
 const Nav = ()=> {
-    return (
-        <nav className={s.Nav}>
-            <div className={`${s.item}`}>
+    return (<div className={s.Nav}>
+        <ul>
+            <li className={`${s.item}`}>
                 <NavLink to="/ProfileCom" activeClassName={s.active}>Profile</NavLink>
-            </div>
-            <div className={s.item}>
+            </li>
+            <li className={s.item}>
                 <NavLink to="/Masseges"activeClassName={s.active} >Massages</NavLink>
-            </div>
-            <div className={s.item}>
+            </li>
+            <li className={s.item}>
                 <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
-            </div>
-            <div className={s.item}>
+            </li>
+            <li className={s.item}>
                 <NavLink to="/setting" activeClassName={s.active}> Setting</NavLink>
-            </div>
-        </nav>);}
+            </li>
+        </ul></div>);}
         export default Nav;
--- src/contonents/Masseges/Masseges.js
+++ src/contonents/Masseges/Masseges.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import s from './Masseges.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Masseges = ()=> {
     return (
         <div className={s["mainMasseges"]}>
             <div className={s["dialogsItem"]}>
-                <div className={`${s.dialog } ${s.active}`}> Igor</div>
+                <div className={`${s.dialog } ${s.active}`}> <NavLink to="/Masseges/1">Igor</NavLink></div>
 
-                <div className={s.dialog}> Oleg</div>
+                <div className={s.dialog}><NavLink to="/Masseges/2"> Oleg</NavLink></div>
 
-                <div className={s.dialog}> Nestor</div>
+                <div className={s.dialog}><NavLink to="/Masseges/3"> Nestor</NavLink></div>
 
-                <div className={s.dialog}> Stepan</div>
+                <div className={s.dialog}><NavLink to="/Masseges/4"> Stepan</NavLink></div>
 
-                <div className={s.dialog}> Taras</div>
+                <div className={s.dialog}><NavLink to="/Masseges/5"> Taras</NavLink></div>
             </div>
             <div className={s.masseges}>
                 <div> Hi!</div>
