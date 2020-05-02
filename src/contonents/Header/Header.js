636ba8083b1b579d74bf9fb7d863b61ec83a839b
Igor Selin <lifesellin@gmail.com>
2020-05-02 16:59:05 +0300

    last

--- src/contonents/Header/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import s from './Header.module.css';
-const Header = ()=> {
-    return (<header className={s.Header}>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Inkscape.logo.svg/390px-Inkscape.logo.svg.png"/>
-    </header>);}
-    export default Header;
--- src/contonents/Masseges/MassegeItem/MassegeItem.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import s from './../Masseges.module.css';
-
-const MessageItem =(props)=>{
-    return( <div> {props.text}</div>);
-}
-        export default MessageItem;
