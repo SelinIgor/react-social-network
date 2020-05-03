58d7900f2451ded9586ddf94976f9b7f7fc33c07
Igor Selin <62283313+SelinIgor@users.noreply.github.com>
2020-05-03 19:20:25 +0300

    blabla

--- src/index.js
+++ src/index.js
@@ -6,15 +6,7 @@ import * as serviceWorker from './serviceWorker';
 let postData =[{massege:'Hello, sabaki! Ya naruto uzumaki', kartinka:'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg', likes:201},
     {massege:'I wanna end me' ,kartinka:'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP1464-CUSA07669_00-AV00000000000005/1586331489000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000' ,likes:15},
     {massege:'Whats wrong with you?' ,kartinka:"https://i.pinimg.com/originals/a9/d0/96/a9d096ac9430a4f297ed99d42861ae9d.jpg" ,likes:64}]
-ReactDOM.render(
-  <React.StrictMode>
-
-    <App postData={postData} />
-
-    <App postData={postData}/>
-
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<App postData={postData} />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
