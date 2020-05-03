9bd748324fcfe5966fe31d68c271092a51d89d48
Igor Selin <62283313+SelinIgor@users.noreply.github.com>
2020-05-03 18:29:07 +0300

    Revert "Update App.js"
    This reverts commit 521dd78c08f2334c44182396640f8fce93164781.

--- src/App.js
+++ src/App.js
@@ -15,10 +15,6 @@ const App = ()=> {
             <div className="app-wrapper">
                 <Header/>
                 <Nav/>
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
                 <div className="app-wrapper-content">
                     <Route path='/masseges' component={Masseges}/>
                     <Route path="/profileCom" component={ProfileCom}/>
