ff107f0d3286dc5f9c247a7fa18e2f9d12fc2116
Igor Selin <62283313+SelinIgor@users.noreply.github.com>
2020-05-04 23:02:46 +0300

    add-img

--- src/contonents/Masseges/DialogItem/DialogItem.js
+++ src/contonents/Masseges/DialogItem/DialogItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import s from './../Masseges.module.css';
+import s from './DialogItem.module.css';
 import {NavLink} from "react-router-dom";
 const DialogItem = (props)=>{
     let path='/Masseges/' + props.id;
     return(
-    <div className={`${s.dialog}` }> <NavLink to={path} activeClassName= {s.active}>{props.name}</NavLink></div>);
+        <div className={`${s.dialog}` }><div className={s.photo}><img  src={props.kartinka} className={s.kartinka}/></div> <div className={s.nameDialog}><NavLink to={path} activeClassName= {s.active} >{props.name}</NavLink></div></div>);
 }
         export default DialogItem;
--- src/redux/state.js
+++ src/redux/state.js
@@ -7,5 +7,8 @@ ProfilePage:{
 },
     MassagePage:{
 masseges :[{text:'Hi there'},{text:'What Am I doing wrong?'},{text:'Nothing. Cant get it'},{text:'Okey'}],
-dialogs :[{name:"Igor", id:1},{name:"Nestor", id:2},{name:"Marta" ,id:3},{name:"Oleg", id:4}]}};
+dialogs :[{name:"Igor", id:1, kartinka:"https://i.pinimg.com/736x/c9/eb/dd/c9ebddca44e1b308c672e641af252be5.jpg"},
+    {name:"Nestor", id:2,kartinka:"https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"},
+    {name:"Marta" ,id:3,kartinka:"https://hahadu.ru/wp-content/uploads/2019/07/3-20.jpg"},
+    {name:"Oleg", id:4,kartinka:"https://focus.ua/storage/pub/images/2017/2615387.jpg"}]}};
 export default state;
