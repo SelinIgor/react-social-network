7428b9d0cb664ba82b06faa35deea10caa96ec2a
Igor Selin <lifesellin@gmail.com>
2020-05-15 16:23:27 +0300

    allready dione

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,54 +1,57 @@
-let RerenderEntireTree = () =>{
-    console.log("State changed");
-}
+let store ={
+    _state : {
+        ProfilePage:{
+            postData :[{massege:'Hello, sabaki! Ya naruto uzumaki', kartinka:'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg', likes:201},
+                {massege:'I wanna end me' ,kartinka:'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP1464-CUSA07669_00-AV00000000000005/1586331489000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000' ,likes:15},
+                {massege:'Whats wrong with you?' ,kartinka:"https://i.pinimg.com/originals/a9/d0/96/a9d096ac9430a4f297ed99d42861ae9d.jpg" ,likes:64}],
+            newPostText:'I think...'
+        },
+        MassagePage:{
+            masseges :[{text:'Hi there'},{text:'What Am I doing wrong?'},{text:'Nothing. Cant get it'},{text:'Okey'}],
+            dialogs :[{name:"Igor", id:1, kartinka:"https://i.pinimg.com/736x/c9/eb/dd/c9ebddca44e1b308c672e641af252be5.jpg"},
+                {name:"Nestor", id:2,kartinka:"https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"},
+                {name:"Marta" ,id:3,kartinka:"https://hahadu.ru/wp-content/uploads/2019/07/3-20.jpg"},
+                {name:"Oleg", id:4,kartinka:"https://focus.ua/storage/pub/images/2017/2615387.jpg"}],
+            newSmsText:'Hello'
+        }},
+    _RerenderEntireTree(){
+        console.log("State changed");
+    },
+    getState(){
+        return this._state;
+    },
+    addPost(){
 
-
-let state = {
-ProfilePage:{
-    postData :[{massege:'Hello, sabaki! Ya naruto uzumaki', kartinka:'https://klike.net/uploads/posts/2019-03/1551511784_4.jpg', likes:201},
-        {massege:'I wanna end me' ,kartinka:'https://store.playstation.com/store/api/chihiro/00_09_000/container/RU/ru/999/EP1464-CUSA07669_00-AV00000000000005/1586331489000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000' ,likes:15},
-        {massege:'Whats wrong with you?' ,kartinka:"https://i.pinimg.com/originals/a9/d0/96/a9d096ac9430a4f297ed99d42861ae9d.jpg" ,likes:64}],
-    newPostText:'I think...'
+        let NewPost ={
+            massege: this._state.ProfilePage.newPostText,
+            kartinka:"https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
+            likes: 12};
+        this._state.ProfilePage.postData.push(NewPost);
+        this._state.ProfilePage.newPostText='';
+        this._RerenderEntireTree(this._state);
+    },
+    changeNewPostText(NewText){
+        this._state.ProfilePage.newPostText = NewText;
+        this._RerenderEntireTree(this._state);
 },
-    MassagePage:{
-masseges :[{text:'Hi there'},{text:'What Am I doing wrong?'},{text:'Nothing. Cant get it'},{text:'Okey'}],
-dialogs :[{name:"Igor", id:1, kartinka:"https://i.pinimg.com/736x/c9/eb/dd/c9ebddca44e1b308c672e641af252be5.jpg"},
-    {name:"Nestor", id:2,kartinka:"https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg"},
-    {name:"Marta" ,id:3,kartinka:"https://hahadu.ru/wp-content/uploads/2019/07/3-20.jpg"},
-    {name:"Oleg", id:4,kartinka:"https://focus.ua/storage/pub/images/2017/2615387.jpg"}],
-        newSmsText:'Hello'
-    }};
-export let addPost = ()=>{
+    addSms(){
+        let NewSms={
+            text: this._state.MassagePage.newSmsText
+        };
+        this._state.MassagePage.masseges.push(NewSms);
+        this._state.MassagePage.newSmsText = '';
+        this._RerenderEntireTree(this._state);
 
-    let NewPost ={
-        massege: state.ProfilePage.newPostText,
-        kartinka:"https://avatarko.ru/img/kartinka/33/multfilm_lyagushka_32117.jpg",
-        likes: 12
-    };
-    state.ProfilePage.postData.push(NewPost);
-    state.ProfilePage.newPostText='';
-    RerenderEntireTree(state);
-};
-export let changeNewPostText = (NewText)=>{
-    state.ProfilePage.newPostText = NewText;
-    RerenderEntireTree(state);
-};
-export let addSms = (addText)=>{
-    let NewSms={
-        text: state.MassagePage.newSmsText
-    };
-    state.MassagePage.masseges.push(NewSms);
-    state.MassagePage.newSmsText = '';
-    RerenderEntireTree(state);
-
+    },
+    changeNewSmsText (NewText){
+        this._state.MassagePage.newSmsText = NewText;
+        this._RerenderEntireTree(this._state);
+    },
+    subscribe(observe){
+        this._RerenderEntireTree= observe;
+    }
 };
 
-export let changeNewSmsText = (NewText)=>{
-    state.MassagePage.newSmsText = NewText;
-    RerenderEntireTree(state);
-};
 
-export const subscribe =(observe)=>{
-RerenderEntireTree= observe;
-}
-export default state;
+
+export default store;
