e6c229849ec0ea0b739d0eae4241a0a93514c99d
Igor Selin <62283313+SelinIgor@users.noreply.github.com>
2020-09-26 13:42:01 +0300

    Updates

---
+++ precache-manifest.a31521d60ffe7c981194e9e3d542ec69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145fa2c4292aaeff320f55be81088fd9",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "a1348dfef735ea35ad72",
+    "url": "/react-social-network/static/css/main.5a747732.chunk.css"
+  },
+  {
+    "revision": "fc4c0bad75cae494eaa2",
+    "url": "/react-social-network/static/js/2.a2cbebe8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social-network/static/js/2.a2cbebe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1348dfef735ea35ad72",
+    "url": "/react-social-network/static/js/main.ec2c3e0c.chunk.js"
+  },
+  {
+    "revision": "4c9556b7ae050c07239e",
+    "url": "/react-social-network/static/js/runtime-main.c471fe11.js"
+  },
+  {
+    "revision": "6e5d6096c02e8c18ad95d40ae5bf0089",
+    "url": "/react-social-network/static/media/prel.6e5d6096.gif"
+  },
+  {
+    "revision": "83bd44a604590ac2ad1d1714185acb72",
+    "url": "/react-social-network/static/media/user.83bd44a6.png"
+  },
+  {
+    "revision": "4f0bc9af570f194ced119a46c5a35d1c",
+    "url": "/react-social-network/static/media/validators.4f0bc9af.bin"
+  }
+]);
